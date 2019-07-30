61ab14947e2a893c786bc92a175446a933cd59a5
rpixel90 <rahulsn@pixel90.onmicrosoft.com>
2019-07-30 11:01:40 +0000

    NewNGCommit

--- README.md
+++ README.md
@@ -28,4 +28,4 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 ## Live Demo
 
-##The Sample Application is running here [Live Demo](https://www.anupajx.com/).
+## The Sample Application is running here [Live Demo](https://www.anupajx.com/).
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
@@ -14,7 +13,7 @@ import { ProductModule } from './products/product.module';
 @NgModule({
   declarations: [ 
     AppComponent,
-    WelcomeComponent 
+    WelcomeComponent
     ],
   imports: [ 
   
